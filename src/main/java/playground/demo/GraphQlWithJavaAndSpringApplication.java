589e97a6aa7c2fb9d4769d608d0a97f207760bce
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-07 11:37:24 -0500

    Initial commit

---
+++ src/main/java/playground/demo/GraphQlWithJavaAndSpringApplication.java
@@ -0,0 +1,13 @@
+package playground.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlWithJavaAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlWithJavaAndSpringApplication.class, args);
+	}
+
+}
