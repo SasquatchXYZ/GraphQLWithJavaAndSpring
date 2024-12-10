c783714900334e3d4245252d50cf7117a1a5fd16
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-09 22:32:02 -0500

    Update controller to retrieve the owner data

--- src/main/java/playground/demo/controllers/PetsController.java
+++ src/main/java/playground/demo/controllers/PetsController.java
@@ -1,19 +1,24 @@
 package playground.demo.controllers;
 
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.reactive.function.client.WebClient;
+import playground.demo.domain.Person;
 import playground.demo.domain.Pet;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @Controller
 public class PetsController {
 
     WebClient petWebClient;
+    WebClient ownerWebClient;
 
     PetsController(WebClient.Builder builder) {
         this.petWebClient = builder.baseUrl("http://pet-service").build();
+        this.ownerWebClient = builder.baseUrl("http://owner-service").build();
     }
 
     @QueryMapping
@@ -22,5 +27,13 @@ public class PetsController {
                 .uri("/pets")
                 .retrieve()
                 .bodyToFlux(Pet.class);
+    }
+
+    @SchemaMapping
+    Mono<Person> owner(Pet pet) {
+        return ownerWebClient.get()
+                .uri("/owner/{id}", pet.ownerId())
+                .retrieve()
+                .bodyToMono(Person.class);
     }
 }
